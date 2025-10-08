f5ce1f53613e159fcf1ff4713c1d287d42e4653d
pulkit-guglani <pulkitname@gmail.com>
2025-10-08 15:20:17 +0530

    Update yarn.lock with new type definitions for React and PropTypes; modify header text in layout.tsx

--- host/app/layout.tsx
+++ host/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html>
       <body suppressHydrationWarning>
-        <header className="p-4 bg-gray-100">Global Header / Sidebar</header>
+        <header className="p-4">Global Header / Sidebar</header>
         <main className="p-4">{children}</main>
       </body>
     </html>
